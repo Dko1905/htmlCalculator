24af2cf1ad5ac1c45af264ff6a0ea0f75f0ac8e8
Daniel Florescu <Daniel.florescu1905@gmail.com>
2019-11-02 13:31:57 +0100

    small changes

--- bare bone/main.js
+++ bare bone/main.js
@@ -4,6 +4,34 @@ function insert(value){
 
 function calculate(){
   let str = document.getElementById("textview").value;
-  let i;
-  
+  if(str){
+    document.getElementById("textview").value = eval(str);
+  }
 }
+
+function delChar(){
+  let str = document.getElementById("textview").value;
+  document.getElementById("textview").value = str.substring(0, str.length-1);
+}
+
+function delAll(){
+  document.getElementById("textview").value = "";
+}
+
+var acceptepleChars = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '(', ')', '%'];
+
+function isCharAccepteble(char){
+  acceptepleChars.forEach((element) => {
+    if(char == element){
+      return true;
+    }
+  })
+  return false;
+}
+
+document.addEventListener('keydown', function(event) {
+  if(isCharAccepteble(event.key)) {
+    insert(event.key);
+  }
+  console.log(event.key)
+});
